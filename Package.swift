6756a63821c5c025dda0a1a4ac184aa1b52b9000
Wayne Dixon <wayne@waynedixon.com>
2023-06-02 16:33:53 -0500

    first commit

---
+++ Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version:5.6
+
+/**
+*  Splash plugin for Publish
+*  Copyright (c) John Sundell 2019
+*  MIT license, see LICENSE file for details
+*/
+
+import PackageDescription
+
+let package = Package(
+    name: "SplashPublishPlugin",
+    platforms: [.macOS(.v12)],
+    products: [
+        .library(
+            name: "SplashPublishPlugin",
+            targets: ["SplashPublishPlugin"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/johnsundell/Publish.git", from: "0.9.0"),
+        .package(url: "https://github.com/johnsundell/Splash.git", from: "0.16.0")
+    ],
+    targets: [
+        .target(
+            name: "SplashPublishPlugin",
+            dependencies: ["Splash", "Publish"]
+        ),
+        .testTarget(
+            name: "SplashPublishPluginTests",
+            dependencies: ["SplashPublishPlugin"]
+        ),
+    ]
+)
